23282fe6613a127c6c678d1f949b9e2b720372c0
merkel2 <merkel@smark.de>
2022-04-05 11:14:13 +0200

    copy funcs

--- src/Scanner.py
+++ src/Scanner.py
@@ -1,10 +1,15 @@
+from cmath import pi
 from distutils.log import error
 from multiprocessing.connection import wait
+from tkinter import Frame
+from turtle import right, window_height
 import cv2
 from time import sleep
 import numpy as np
 from Structs import Pinout, Resolutions
 import RPi.GPIO as GPIO
+from main import BorderExpectation, Cropfactor
+from scipy.stats import rankdata
 
 
 class Scanner:
@@ -76,3 +81,79 @@ class Scanner:
         GPIO.output(Pinout.SHUTTER, GPIO.LOW)
         sleep(self.waitingTime)
         return counter
+
+    def functionFAST(self, xx, yy, a, b, c, wait):
+        Pinout.DIR()
+        # todo
+
+    def maxEdge(self, FrameProjection, position):
+        if self.orientation == 0:
+            FrameLimit = len(FrameProjection)
+            EdgeValue = 5
+        else:
+            FrameLimit = len(FrameLimit)
+            EdgeValue = 5
+
+        if min(position) >= 0 + EdgeValue:
+            a = float(FrameProjection[min(position)])
+            b = float(FrameProjection[min(position) - 3])
+
+        if max(position) <= FrameLimit-EdgeValue:
+            a = float(FrameProjection[max(position)])
+            b = float(FrameProjection[min(position) - 3])
+
+        if min(position) <= EdgeValue or max(position) >= FrameLimit-EdgeValue:
+            return -1, 1
+        else:
+            return left, right
+
+    def captureData(self):
+        video = cv2.VideoCapture(2)
+        sleep(0.9)
+        check, frame = video.read()
+        height = len(frame[:, 0])
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        FrameProjection = np.sum(frame, axis=self.orientation)
+
+        CropVertical = round(height*self.Cropfactor)
+        b = max(np.argwhere(FrameProjection > 0))-5
+        a = np.argmax(FrameProjection > 0)+5
+        frame = frame[0+CropVertical:height-CropVertical, a:int(b)]
+        FrameProjection = FrameProjection[a:int(b)]
+        if self.colorNegative == 0:
+            frame = -frame
+            FrameProjection = max(FrameProjection)-FrameProjection
+
+        maskHor = (FrameProjection/max(FrameProjection)) > self.threshold
+
+        position = np.where(maskHor == 1)[0]
+        items = self.label(position)
+
+        a = np.array([(x,
+                       len(position[items == x]),
+                       round(np.median(position[items == x])),
+                       max(np.sum(frame, axis=self.orientation)[position[items == x]]))
+                      for i, x in enumerate(np.unique(items))])
+
+        PFramelength, LFramelength = np.shape(frame)
+        return a, FrameProjection, position, items, PFramelength, LFramelength
+
+    def CheckingEdgeDeciding(self, aa, FrameProjection, position, items, latch, BackupPictureTage):
+        if self.orientation == 0:
+            a = 11
+            idealBoderSize = 22
+        else:
+            a = 22
+            idealBoderSize = 44
+        aa[:, 1] = abs(aa[:, 1]-idealBoderSize)
+        Positonselected = 0
+        weights = [[1], [4], [1]]
+        if latch == 0:
+            aa[:, 2] = abs(aa[:, 2]-round(LFramelenght/BorderExpectation))
+        elif latch == 2:
+            aa[:, 2] = abs(aa[:, 2]-(BorderExpectation-1.5) /
+                           BorderExpectation*LFramelength)
+
+        decide = sum([rankdata(-aa[:, 1]), rankdat(-aa[:, 2]),
+                     rankdata(aa[:, 3])*np.array(weights)])
+        Itemselected = aa[decide == max(decide), 0]
--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import cv2
 import time
 import numpy as np
-from scipy.stats import rankdata
 
 from Scanner import Scanner
 
